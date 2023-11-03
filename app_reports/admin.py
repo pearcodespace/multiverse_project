4f6c089a65f10c45930282c126a57634854546a7
proud55 <100156592+proud55@users.noreply.github.com>
2023-11-03 14:29:49 +0000

    update

--- app_reports/admin.py
+++ app_reports/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+@admin.register(ClothDescription)
+class ClothDescription(admin.ModelAdmin):
+    pass
--- app_reports/models.py
+++ app_reports/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class ClothDescription(models.Model):
+    Cloth_ID = models.IntegerField()
+    Age = models.IntegerField()
+    Review_Text = models.CharField(max_length=255)
+    Rating = models.IntegerField()
+    Positive_Feedback_Count = models.IntegerField()
+    Department_Name = models.CharField(max_length=255)
